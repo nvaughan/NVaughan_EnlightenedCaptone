0e0b64c766b201f0b6cef18f849218bdb6238429
nvaughan <nvaughan@student.neumont.edu>
2014-02-18 23:25:00 -0700

    Updated Sender

--- Code/XBeeTestSender/XBeeTestSender.ino
+++ Code/XBeeTestSender/XBeeTestSender.ino
@@ -1,20 +1,24 @@
-//http://lab.guilhermemartins.net/2008/12/24/serial-comunication-between-arduinos-with-wire-wireless/
+//http://lab.guilhermemartins.net/2013/08/01/new-basic-arduino-serial-communication/
+// SENDER
+int b1 = 7;
+int b2 = 8;
 
-int analogValue5, val5;
-
-void setup() {
-// Serial port enable
-Serial.begin(19200);
+void setup()
+{
+Serial.begin(9600);
+pinMode(b1, INPUT);
+pinMode(b2, INPUT);
+digitalWrite(b1, HIGH);
+digitalWrite(b2, HIGH);
 }
 
-void loop() {
-// read analog pin 5
-analogValue5 = analogRead(5);
-
-// remap values from the analogValue5 variable to 0 / 255
-val5 = map(analogValue5, 0, 1023, 0, 255);
-
-// send the value to the serial port
-Serial.println(val5, BYTE);
-
+void loop()
+{
+byte val1 = (byte)digitalRead(b1);
+byte val2 = (byte)digitalRead(b2);
+Serial.write(‘a’); //SYNC char
+Serial.write(val1);
+Serial.write(‘b’); //SYNC char
+Serial.write(val2);
+delay(50);
 }
