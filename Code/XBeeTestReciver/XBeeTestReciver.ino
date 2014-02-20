b9f3f790aebaadb28fc279131a198849f5a30f2f
nvaughan <nvaughan@student.neumont.edu>
2014-02-20 15:02:13 -0700

    Update

--- Code/XBeeTestReciver/XBeeTestReciver.ino
+++ Code/XBeeTestReciver/XBeeTestReciver.ino
@@ -11,35 +11,44 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available() > 0)
+  if(Serial.available())
   {
     char inChar = Serial.read();
+    Serial.write(inChar);
     while(Serial.available()<=0);
+    Serial.write(inChar);
     if(inChar=='a')
     {
+      Serial.write(inChar);
       strip.setPixelColor(0,155,155,155);
       strip.setPixelColor(1,155,155,155);
       strip.setPixelColor(2,155,155,155);
       strip.setPixelColor(3,0,0,0);
       strip.setPixelColor(4,0,0,0);
       strip.setPixelColor(5,0,0,0);
+      strip.show();
     }
-    
+
     if(inChar == 'b')
     {
+      Serial.write(inChar);
       strip.setPixelColor(0,0,0,0);
       strip.setPixelColor(1,0,0,0);
       strip.setPixelColor(2,0,0,0);
       strip.setPixelColor(3,155,155,155);
       strip.setPixelColor(4,155,155,155);
       strip.setPixelColor(5,155,155,155);
+      strip.show();
     }
   }
-  
+
   strip.show();
 
   delay(10);
 }
+
+
+
 
 
 
